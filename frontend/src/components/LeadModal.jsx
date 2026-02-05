aff5a833ee26987783d5bd1c39b61722c7e6e86f
Kota-James <n.kota.all.01@gmail.com>
2026-02-05 20:43:40 +0900

    bug-fix

--- frontend/src/components/LeadModal.jsx
+++ frontend/src/components/LeadModal.jsx
@@ -58,7 +58,7 @@ const LeadModal = ({active, handleModal, token, id, setErrorMessage}) => {
                 note: note, 
             })
         };
-        const response = await fetch(`/api/leads/${id}`, requestOptions);
+        const response = await fetch(`/api/leads`, requestOptions);
         if(!response.ok){
             setErrorMessage("Something went wrong when creating lead");
         }else{
@@ -96,7 +96,7 @@ const LeadModal = ({active, handleModal, token, id, setErrorMessage}) => {
         <div className={`modal ${active && "is-active"}`}>
             <div className="modal-background" onClick={handleModal}></div>
             <div className="modal-card">
-                <header className="modal-card-head has-background-primary-light">
+                <header className="modal-card-head has-background-gray-dark">
                     <h1 className="modal-card-title">
                         { id ? "update Lead" : "Create Lead"}
                     </h1>
@@ -168,7 +168,7 @@ const LeadModal = ({active, handleModal, token, id, setErrorMessage}) => {
                     </form>
 
                 </section>
-                <footer className="modal-card-foot has-background-primary-light">
+                <footer className="modal-card-foot has-background-gray-dark">
                     {id ? (
                         <button className="button is-info" onClick={handleUpdateLead}>
                             Update
